08c41e4742243286e24345af15b029f380ec7d25
Alekson Fortes <alekson@klue.com>
2024-05-07 12:56:48 -0400

    Finish all the basic tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,25 +2,42 @@ describe('Test suite for Homepage', () => {
 
   const Homepage = 'http://localhost:3000/';
 
-  it('Renderiza o cabeçalho com um h1 e alternância de tema', () => {
+  beforeEach(() => {
     cy.visit(Homepage);
+  })
+
+  it('Renders the header with an h1 and theme toggle', () => {
     cy.get('h1').should('have.text', 'EngageSphere');
     cy.get('#theme-toggle-button').should('be.visible');
   })
 
-  it('Mostra a saudação padrão (ou seja, Hi there! ...)', () => {
+  it('Shows the default greeting (i.e., Hi there! ...)', () => {
+    cy.get('[data-testid="table"] > :nth-child(1)').should('contain.text', 'Hi there!');
   })
 
-  it('Mostra uma saudação personalizada (por exemplo, Hi Joe! ...)', () => {
+  it('Shows a customized greeting (e.g., Hi Joe! ...)', () => {
+    let name = 'Alekson';
+    cy.get('[data-testid="name"]').type(name);
+    cy.get('[data-testid="table"] > :nth-child(1)').should('contain.text', `Hi ${name}!`);
   })
 
-  it('Desativa o campo de entrada de texto quando na página de detalhes do cliente', () => {
+  it('Disables the text input field when in the customer details page', () => {
+    cy.get('[data-testid="table"] tr:first-child').find('td:first-child').click();
+    cy.get('[data-testid="name"]').should('be.disabled');
   })
 
-  it('olta para a lista de clientes ao clicar no botão "Voltar"', () => {
+  it('Goes back to the customers list when clicking the "Back" button', () => {
+    cy.get('[data-testid="table"] tr:first-child').find('td:first-child').click();
+    cy.contains('button', 'Back').click();
+    cy.get('[data-testid="table"] > :nth-child(2)').should('have.text', 'Below is our customer list.');
   })
 
-  it('Mostra o rodapé e seus links', () => {
+  it('Shows the footer and its links', () => {
+    cy.get('[data-testid="footer"] p').should('have.text', 'Copyright 2024 - Talking About Testing');
+    cy.get('[data-testid="footer"] a').should('have.length', 3);
+    cy.get('[data-testid="footer"] a').eq(0).should('have.attr', 'href', 'https://udemy.com/user/walmyr').and('contain.text', 'Udemy');
+    cy.get('[data-testid="footer"] a').eq(1).should('have.attr', 'href', 'https://talkingabouttesting.com').and('contain.text', 'Blog');
+    cy.get('[data-testid="footer"] a').eq(2).should('have.attr', 'href', 'https://youtube.com/@talkingabouttesting').and('contain.text', 'YouTube');
   })
 
 })
