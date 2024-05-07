72e718ab020ee13bd85359623465e97d0a84fd3b
jonathanrsbr <jonathanrsbr@gmail.com>
2024-05-07 19:59:35 -0300

    feat(api): Add API custom command getCustomers and API tests structure for api.cy.js

--- cypress/e2e/api/api.cy.js
+++ cypress/e2e/api/api.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('API Testing Suite for the /customers Endpoint', () => {
+  it('successfully retrieves customers', () => {
+    cy.getCustomers().then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+  it('Deve paginar a lista de clientes corretamente', () => {
+    cy.paginateCustomers();
+  });
+
+  it('Deve filtrar clientes por tamanho corretamente', () => {
+    cy.filterCustomersBySize('large');
+  });
+
+  it('Deve verificar a estrutura correta da resposta', () => {
+    cy.checkResponseStructure();
+  });
+
+  it('Deve tratar solicitações inválidas de forma elegante', () => {
+    cy.handleInvalidRequests();
+  });
+});
