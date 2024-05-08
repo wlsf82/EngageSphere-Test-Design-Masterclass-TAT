fb69648d36f59277dff0c59e230692104662e5b9
jonathanrsbr <jonathanrsbr@gmail.com>
2024-05-08 15:03:54 -0300

    feat(api): Add getPaginatedCustomers custom command and customer pagination test

--- cypress/e2e/api/api.cy.js
+++ cypress/e2e/api/api.cy.js
@@ -1,20 +1,34 @@
 describe('API Testing Suite for the /customers Endpoint', () => {
   it('successfully retrieves customers', () => {
-    cy.getCustomers().then((response) => {
+    cy.getCustomers().then(response => {
       expect(response.status).to.eq(200);
       expect(response.body.customers).to.be.an('array').that.is.not.empty;
-      response.body.customers.forEach((customer) => {
-              return Cypress._.isNumber(customer.id) &&
-               Cypress._.isString(customer.name) &&
-               Cypress._.isNumber(customer.employees) &&
-               Cypress._.isString(customer.size) &&
-               Cypress._.isObject(customer.address);
+      response.body.customers.forEach(customer => {
+        return Cypress._.isNumber(customer.id) &&
+        Cypress._.isString(customer.name) &&
+        Cypress._.isNumber(customer.employees) &&
+        Cypress._.isObject(customer.contactInfo) &&
+        Cypress._.isObject(customer.address) &&
+        Cypress._.isString(customer.size) &&
+        ['Small', 'Medium', 'Enterprise', 'Large Enterprise', 'Very Large Enterprise'].includes(customer.size) &&
+        (customer.contactInfo && Cypress._.isString(customer.contactInfo.name) && Cypress._.isString(customer.contactInfo.email)) &&
+        (customer.address && Cypress._.isString(customer.address.street));
       });
     });
   });
 
   it('Paginates the customer list correctly', () => {
-    cy.paginateCustomers();
+    cy.getPaginatedCustomers({ page: 2, limit: 20, size: 'Enterprise' }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('pageInfo').that.exist;
+  
+      const { pageInfo, customers } = response.body;
+  
+      expect(pageInfo.currentPage).to.eq('2');
+      expect(pageInfo.totalPages).to.eq(1);
+      expect(pageInfo.totalCustomers).to.be.gte(1);
+      expect(customers).to.be.an('array').and.to.have.lengthOf(0);
+    });
   });
 
   it('Filters customers by size correctly', () => {
@@ -30,18 +44,18 @@ describe('API Testing Suite for the /customers Endpoint', () => {
   });
 
   it('handles invalid requests gracefully (negative limit)', () => {
-    // Test logic to make an invalid request with a negative limit
+    
   });
 
   it('handles invalid requests gracefully (page as a string)', () => {
-    // Test logic to make an invalid request with page as a string
+    
   });
 
   it('handles invalid requests gracefully (limit as a boolean)', () => {
-    // Test logic to make an invalid request with limit as a boolean
+    
   });
 
   it('handles invalid requests gracefully (unsupported size)', () => {
-    // Test logic to make an invalid request with an unsupported size
+    
   });
 });
--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,3 +1,19 @@
 Cypress.Commands.add('getCustomers', () => {
-    return cy.api('GET', Cypress.config().backendUrl + '/customers');
+    return cy.api({
+        method: 'GET',
+        url: Cypress.config().backendUrl + '/customers'     
+    });
 });
+
+Cypress.Commands.add('getPaginatedCustomers', ({ page, limit, size }) => {
+    const queryParams = {};
+    if (page) queryParams.page = page;
+    if (limit) queryParams.limit = limit;
+    if (size) queryParams.size = size;
+  
+    cy.api({
+      method: 'GET',
+      url: Cypress.config().backendUrl + '/customers',
+      qs: queryParams
+    });
+  });
