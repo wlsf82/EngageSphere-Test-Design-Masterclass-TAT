b36a33f742ec63a5046ffe212cfff8c761e4ce2a
leticia <leticia.nurnberg@multipedidos.com.br>
2024-05-08 21:17:33 -0300

    modified:   exercicios.spec.js

--- cypress/exercicios.spec.js
+++ cypress/exercicios.spec.js
@@ -3,7 +3,7 @@ describe('Recuperação de Clientes', () => {
       // Fazer a chamada à API para recuperar clientes
       cy.request('GET', 'http://localhost:3001/customers?page=2&limit=10&size=All')
         .then((response) => {
-          // Verificar se o código de status é 200 - teste
+          // Verificar se o código de status é 200 - teste deus me ajuda
           expect(response.status).to.equal(200);
         });
     });
