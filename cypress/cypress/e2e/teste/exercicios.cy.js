96224ebe906a4c310abc225cd85df771645521ad
leticia <leticia.nurnberg@multipedidos.com.br>
2024-05-09 11:24:24 -0300

    new file:   cypress.config.js 	renamed:    exercicios.spec.js -> cypress/e2e/teste/exercicios.cy.js 	new file:   cypress/fixtures/example.json 	new file:   cypress/support/commands.js 	new file:   cypress/support/e2e.js

--- cypress/cypress/e2e/teste/exercicios.cy.js
+++ cypress/cypress/e2e/teste/exercicios.cy.js
@@ -1,11 +1,12 @@
+// Recupera clientes com sucesso (por exemplo, verifica o código de status 200) *
 describe('Recuperação de Clientes', () => {
     it('Deve recuperar clientes com sucesso', () => {
-      // Fazer a chamada à API para recuperar clientes
       cy.request('GET', 'http://localhost:3001/customers?page=2&limit=10&size=All')
         .then((response) => {
-          // Verificar se o código de status é 200 
           expect(response.status).to.equal(200);
         });
     });
   });
+
+// Pagina a lista de clientes corretamente
 
