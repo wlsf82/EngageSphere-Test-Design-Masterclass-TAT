8f8912ca2f6cfd9bf06934610dd60bac7a5d0c8b
krugio <baurudoporto@gmail.com>
2024-05-12 15:23:06 -0300

    Atualizacao numero de linhas do command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,6 @@ const selectFilterBySize = 'select[data-testid="filter"]'
 const tableHeadSize = 'tbody tr td:nth-child(4)'
 
 Cypress.Commands.add('apiFilterBySize', ({ size }) => {
-
     cy.request(`http://localhost:3001/customers?page=1&limit=10&size=${size}`).then((response) => {
 
         const customers = response.body.customers;
@@ -11,31 +10,20 @@ Cypress.Commands.add('apiFilterBySize', ({ size }) => {
         // Faco um filter para ver se tenho pelo menos 1 Size
         const smallCustomers = customers.filter(customer => customer.size === size);
         expect(smallCustomers).to.have.lengthOf.at.least(1);
-
-
     })
 
 })
 
-
-
-
 Cypress.Commands.add('filterRecordsBySize', ({ size }) => {
-
     const listOfSizesFiltered = []
-
     cy.get(selectFilterBySize, { timeout: 30000 }).should('be.visible').select(`${size}`).then(() => {
-
         cy.wait(2000)
-       
         cy.get(tableHeadSize).then(tdElements => {
-
             for (let i = 0; i < tdElements.length; i++) {
                 cy.wrap(tdElements.eq(i)).invoke('text').then(text => {
                     listOfSizesFiltered.push(text)
                 });
             }
-
 
             cy.wrap(listOfSizesFiltered).then((response) => {
                 if (size === 'All') {
@@ -51,15 +39,7 @@ Cypress.Commands.add('filterRecordsBySize', ({ size }) => {
                 }
             })
 
-
         })
-
-
     })
-
-
-
-
-
 
 })
