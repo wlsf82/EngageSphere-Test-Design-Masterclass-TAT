ae4775f70dfcaffc11220d4d959b2f3b450abe95
krugio <baurudoporto@gmail.com>
2024-05-12 15:43:39 -0300

    Atualizacao filterExportation

--- cypress/e2e/GUI/filterExportation.cy.js
+++ cypress/e2e/GUI/filterExportation.cy.js
@@ -4,14 +4,20 @@ describe('Filtros e Exportação', () => {
     });
    
     it('Verificar se é possível filtrar por cada tamanho (All, Small, Medium, Enterprise, Large Enterprise, e Very Large Enterprise)', () => { 
+       
+        cy.intercept(
+            'GET',
+            `${Cypress.env('baseUrlApi')}/customers?page=1&limit=10&size=Small`,
+           
+          ).as('getSmallCustomers')
         
-        cy.filterRecordsBySize({size:'All'})
-        cy.filterRecordsBySize({size:'Small'})
-        cy.filterRecordsBySize({size:'Medium'})
-        cy.filterRecordsBySize({size:'Enterprise'})
-        cy.filterRecordsBySize({size:'Large Enterprise'})
-        cy.filterRecordsBySize({size:'Very Large Enterprise'})
-
+          cy.get('[data-testid="filter"]').select('Small')
+         
+       
+          cy.wait('@getSmallCustomers')
+        
+          cy.get('tbody tr').should('have.length.gt',1)
+        
     });
     
     it('Baixa corretamente uma lista de clientes como um arquivo CSV', () => { 
