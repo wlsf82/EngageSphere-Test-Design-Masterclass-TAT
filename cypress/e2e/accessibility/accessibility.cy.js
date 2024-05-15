22d795d6b680ef88af743af39f45fdddfa7bb604
jonathanrsbr <jonathanrsbr@gmail.com>
2024-05-15 17:03:30 -0300

    refactor(accessibility): Delete the test files and merge all cases into a single file, refactor the custom command

---
+++ cypress/e2e/accessibility/accessibility.cy.js
@@ -0,0 +1,31 @@
+describe('Accessibility Tests', () => {
+	context('Customer Table', () => {
+		it('finds no a11y issues in light mode', () => {
+			cy.accessibilityCheck()
+		})
+
+		it('finds no a11y issues in dark mode', () => {
+			cy.accessibilityCheck({ mode: 'dark' })
+		})
+	})
+
+	context('Customer Details', () => {
+		it('finds no a11y issues in light mode', () => {
+			cy.accessibilityCheck({ showDetails: true })
+		})
+
+		it('finds no a11y issues in dark mode', () => {
+			cy.accessibilityCheck({ mode: 'dark', showDetails: true })
+		})
+	})
+
+	context('Show Address', () => {
+		it('finds no a11y issues in light mode', () => {
+			cy.accessibilityCheck({ showDetails: true, showAddress: true })
+		})
+
+		it('finds no a11y issues in dark mode', () => {
+			cy.accessibilityCheck({ mode: 'dark', showDetails: true, showAddress: true })
+		})
+	})
+})
