594f0ce7dac6c38f3bd3571e933183e40527c938
jonathanrsbr <jonathanrsbr@gmail.com>
2024-05-15 17:18:33 -0300

    chore(setup): Add sensitive files to gitignore, delete example.json from fixtures folder and gui_commands.js file

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,6 @@ module.exports = defineConfig({
 		env: {
 			API_URL: 'http://localhost:3001'
 		},
-
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		}
+		fixturesFolder: false
 	}
 })
