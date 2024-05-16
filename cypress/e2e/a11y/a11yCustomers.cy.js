13da6fe166091efaf7924e3af2044746fb709f7c
L4nd1m <landarthur@gmail.com>
2024-05-16 00:30:57 -0300

    Update a11yCustomers.cy.js
    Deixei todos os its dentro do mesmo describe para aproveitar hooks e desfiz o comando customizado, passando o inject no before each e o check dentro de cada it

--- cypress/e2e/a11y/a11yCustomers.cy.js
+++ cypress/e2e/a11y/a11yCustomers.cy.js
@@ -1,42 +1,41 @@
-describe('A11y Customers table', () => {
+describe('A11y Customers table and detail', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000')
-    
+        cy.visit('/')
+        cy.injectAxe()
       })
 
     it('não encontra problemas de acessibilidade no modo claro', () => {
-        cy.a11yCheck()
+       cy.checkA11y()
+       
     })
     it('não encontra problemas de acessibilidade no modo escuro', () => {
         cy.get('#theme-toggle-button').click()
         cy.get('body').should('have.attr','data-theme','dark')
-        cy.a11yCheck()
+       cy.checkA11y()
+       
     })
-})
 
-describe('A11y Customers detail', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000')
-    
-      })
-      it('não encontra problemas de acessibilidade no modo claro', () => {
+    it('não encontra problemas de acessibilidade no modo claro', () => {
         cy.get('table').find('tr').eq(1).click()
         cy.get('.customer-details').should('be.visible')
-        cy.a11yCheck()
+       cy.checkA11y()
+       
     })
     it('não encontra problemas de acessibilidade no modo escuro', () => {
         cy.get('table').find('tr').eq(1).click()
         cy.get('.customer-details').should('be.visible')
         cy.get('#theme-toggle-button').click()
         cy.get('body').should('have.attr','data-theme','dark')
-        cy.a11yCheck()
+       cy.checkA11y()
+       
     })
     it('mostrar endereço -> não encontra problemas de acessibilidade no modo claro', () => {
         cy.get('table').find('tr').eq(1).click()
         cy.get('.customer-details').should('be.visible')
         cy.get('.show-address-btn').should('be.visible').click()
         cy.get('.address-info').should('be.visible')
-        cy.a11yCheck()
+       cy.checkA11y()
+       
     })
     it('mostrar endereço -> não encontra problemas de acessibilidade no modo claro', () => {
         cy.get('table').find('tr').eq(1).click()
@@ -45,6 +44,7 @@ describe('A11y Customers detail', () => {
         cy.get('.address-info').should('be.visible')
         cy.get('#theme-toggle-button').click()
         cy.get('body').should('have.attr','data-theme','dark')
-        cy.a11yCheck()
+       cy.checkA11y()
+       
     })
 })
