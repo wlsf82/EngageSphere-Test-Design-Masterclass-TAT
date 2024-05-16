d317f08d65b86185f66c4505f0dc71de1c577590
L4nd1m <landarthur@gmail.com>
2024-05-15 23:51:37 -0300

    Update cypress.config.js
    baseURL passa a ter URL do frontend como valor, nova propriedade env com URL da API e  remoção de configurações de video e trashassetsbeforeruns por estarem definidas com o valor default

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:3000",
+
+    env: {
+			API_URL: 'http://localhost:3001'
+		},
   },
 
   fixturesFolder: false,
-  video: false,
-  trashAssetsBeforeRuns: true,
 
   component: {
     
