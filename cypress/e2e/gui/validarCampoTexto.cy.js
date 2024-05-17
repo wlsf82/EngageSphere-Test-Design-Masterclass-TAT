45276dbf71075fdcd3b2969a29e27b90ebcbe213
sanychiquetti <sanymara77@hotmail.com>
2024-05-17 12:30:55 +0200

    corrigindo fork

---
+++ cypress/e2e/gui/validarCampoTexto.cy.js
@@ -0,0 +1,14 @@
+describe('Testes dos componentes da aplicação Engage Sphere', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/customers**').as('getCustomers')
+    cy.visit('/customers?page=1&limit=10&size=All')
+    cy.wait('@getCustomers')
+  })
+  it('Desativa o campo de entrada de texto quando na página de detalhes do cliente', () => {
+    cy.get('table tr td button')
+      .first()
+      .click()
+    cy.get('[data-testid="name"]')
+      .should('be.disabled')
+  })
+})
---
+++ cypress/e2e/gui/validarFallBackLoading.cy.js
@@ -0,0 +1,10 @@
+describe('Validar a mensagem de "Loading"', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/customers**').as('getCustomers')
+    cy.visit('/customers?page=1&limit=10&size=All')
+    cy.wait('@getCustomers')
+  })
+  it('Deve exibir a mensagem de carregamento', () => {
+    cy.get('#loading').should('be.visible')
+  })
+})
