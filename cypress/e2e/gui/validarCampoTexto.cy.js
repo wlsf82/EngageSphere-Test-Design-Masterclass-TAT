afcbcec2c7cbe2570f569141955d9bde8aa044b3
sanychiquetti <sanymara77@hotmail.com>
2024-05-18 18:30:44 +0200

    corrigindo endereço cy.visit e a busca de elemento arquivo 'validarCampoTexto'

--- cypress/e2e/gui/validarCampoTexto.cy.js
+++ cypress/e2e/gui/validarCampoTexto.cy.js
@@ -1,12 +1,12 @@
 describe('Validar o campo de texto da aplicação Engage Sphere', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/customers**').as('getCustomers')
-    cy.visit('customers?page=1&limit=10&size=All')
+    cy.visit('')
     cy.wait('@getCustomers')
   })
-  
+
   it('Desativa o campo de entrada de texto quando na página de detalhes do cliente', () => {
-    cy.get('table tr td button')
+    cy.get('tbody tr')
       .first()
       .click()
     cy.get('[data-testid="name"]')
