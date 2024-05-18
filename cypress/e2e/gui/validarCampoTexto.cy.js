9af71e407ccd8b5749dd6df9c1022eaf7b39aeb1
sanychiquetti <sanymara77@hotmail.com>
2024-05-18 18:21:34 +0200

    renomeando o describe do arquivo 'valida CampoTexto' para ficar mais condinzente com o teste em questão E para melhora a legibilidade do código inserindo uma linha entre as funções

--- cypress/e2e/gui/validarCampoTexto.cy.js
+++ cypress/e2e/gui/validarCampoTexto.cy.js
@@ -1,9 +1,10 @@
-describe('Testes dos componentes da aplicação Engage Sphere', () => {
+describe('Validar o campo de texto da aplicação Engage Sphere', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/customers**').as('getCustomers')
     cy.visit('customers?page=1&limit=10&size=All')
     cy.wait('@getCustomers')
   })
+  
   it('Desativa o campo de entrada de texto quando na página de detalhes do cliente', () => {
     cy.get('table tr td button')
       .first()
