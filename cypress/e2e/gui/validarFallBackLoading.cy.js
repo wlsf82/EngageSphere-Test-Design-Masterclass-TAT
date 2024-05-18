02df53ebd003c06ddb32f2f65072e2ccacc1f9bc
sanychiquetti <sanymara77@hotmail.com>
2024-05-18 17:10:24 +0200

    tornando o teste 'validar a mensagem de Loading' determinístico

--- cypress/e2e/gui/validarFallBackLoading.cy.js
+++ cypress/e2e/gui/validarFallBackLoading.cy.js
@@ -1,10 +1,11 @@
 describe('Validar a mensagem de "Loading"', () => {
   beforeEach(() => {
-    cy.intercept('GET', '**/customers**').as('getCustomers')
-    cy.visit('customers?page=1&limit=10&size=All')
-    cy.wait('@getCustomers')
+    cy.intercept('GET', '**/customers**', { delay:1000 }).as('getCustomers')
+    cy.visit('')
   })
   it('Deve exibir a mensagem de carregamento', () => {
     cy.get('#loading').should('be.visible')
+    cy.wait('@getCustomers')
+    cy.get('#Loading').should('not.exist')
   })
 })
