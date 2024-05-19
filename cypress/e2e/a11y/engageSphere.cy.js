9c7743c93ed2cbd3bc05c0564b81c35a0ad43942
sanychiquetti <sanymara77@hotmail.com>
2024-05-19 16:47:32 +0200

    acrescentatando testes finais de acessibilidade

--- cypress/e2e/a11y/engageSphere.cy.js
+++ cypress/e2e/a11y/engageSphere.cy.js
@@ -8,9 +8,52 @@ describe('Validar acessibilidade da aplicação Engage Sphere', () => {
     cy.checkA11y()
   })
 
-  it.only('Não encontra problemas de acessibilidade no modo escuro', () => {
-    cy.get('#theme-toggle-button').click()
+  it('Não encontra problemas de acessibilidade no modo escuro', () => {
+    cy.get('#theme-toggle-button')
+      .should('be.visible')
+      .click()
     cy.get('body').should('have.attr', 'data-theme', 'dark')
     cy.checkA11y()
+  })
+
+  context('Detalhes do cliente', () => {
+    beforeEach(() => {
+      cy.get('tbody tr')
+        .should('be.visible')
+        .first()
+        .click()
+    })
+
+    it('Não encontra problemas de acessibilidade no modo claro', () => {
+      cy.checkA11y()
+    })
+
+    it('Não encontra problemas de acessibilidade no modo escuro', () => {
+      cy.get('#theme-toggle-button')
+        .should('be.visible')
+        .click()
+      cy.get('body').should('have.attr', 'data-theme', 'dark')
+      cy.checkA11y()
+    })
+
+    context('Mostrar endereço', () => {
+      beforeEach(() => {
+        cy.get('.show-address-btn')
+          .should('be.visible')
+          .click()
+      })
+
+      it('Não encontra problemas de acessibilidade no modo claro', () => {
+        cy.checkA11y()
+      })
+
+      it('Não encontra problemas de acessibilidade no modo escuro', () => {
+        cy.get('#theme-toggle-button')
+          .should('be.visible')
+          .click()
+        cy.get('body').should('have.attr', 'data-theme', 'dark')
+        cy.checkA11y()
+      })
+    })
   })
 })
