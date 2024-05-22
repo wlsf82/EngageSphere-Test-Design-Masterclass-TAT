479a9c155447010bdc2b4da6169d6c01d5ec13df
leticia <leticia.nurnberg@multipedidos.com.br>
2024-05-22 19:58:41 -0300

    Ajustando pastas e movendo o arquivo de configuração do cypress  para a raiz

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    API_URL: "http://localhost:3001/"
+  },
+  e2e: {
+    baseUrl: "http://localhost:3000/",
+    supportFile: false,
+    fixturesFolder: false,
+  },
+});
