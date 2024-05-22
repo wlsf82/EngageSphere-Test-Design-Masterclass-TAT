d0664263085aa51ead0638b7463b83c99a05abf5
L4nd1m <51890535+L4nd1m@users.noreply.github.com>
2024-05-22 18:50:03 -0300

    Removendo linha cypress.config.js
    Co-authored-by: Walmyr <wlsf82@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-
     env: {
 			API_URL: 'http://localhost:3001'
 		},
