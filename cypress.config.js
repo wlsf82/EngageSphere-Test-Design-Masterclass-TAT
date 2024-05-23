e6a2bed063689d5869032fdaebad7a5bcb4e83e9
leticia <leticia.nurnberg@multipedidos.com.br>
2024-05-22 22:42:43 -0300

    Your branch is up to date with 'origin/main'. 	modified:   cypress.config.js 	modified:   cypress/e2e/EngageSphere_API.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env: {
-    API_URL: "http://localhost:3001/"
+    API_URL: "http://localhost:3001"
   },
   e2e: {
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "http://localhost:3000",
     supportFile: false,
     fixturesFolder: false,
   },
--- cypress/e2e/EngageSphere_API.cy.js
+++ cypress/e2e/EngageSphere_API.cy.js
@@ -1,20 +1,19 @@
-// Recupera clientes com sucesso (por exemplo, verifica o código de status 200)
-describe('Recuperação de Clientes', () => {
-    it('Deve recuperar clientes com sucesso', () => {
+const CUSTOMERS_API_URL= `${Cypress.env('API_URL')}/customers`;
+
+
+describe('GET /customers', () => {
+  it('Retorna o código de status 200 em uma requisição bem sucedida', () => {
       cy.request('GET', 'http://localhost:3001/customers?page=2&limit=10&size=All')
-        .then((response) => {
-          expect(response.status).to.equal(200);
+      .then(({ status }) => {
+        expect(status).to.equal(200);
         });
     });
   });
 
-// Pagina a lista de clientes corretamente
-
 describe('Teste de Listagem de Clientes', () => {
     it('Deve retornar a lista de clientes com sucesso', () => {
-      cy.request('GET', 'http://localhost:3001/customers?page=1&limit=10&size=All').then((response) => {
+      cy.request('GET', `${Cypress.env('API_URL')}/customers`).then((response) => {
         expect(response.status).to.equal(200);
-
         expect(response.body).to.have.all.keys('customers', 'pageInfo');
         expect(response.body.customers).to.have.length.gt(0);
         expect(response.body.pageInfo).to.have.all.keys('currentPage', 'totalPages', 'totalCustomers');
@@ -22,41 +21,18 @@ describe('Teste de Listagem de Clientes', () => {
     });
   });
 
-//Filtra clientes por tamanho corretamente
+  describe('Teste de filtro de clientes por tamanho', () => {
+  it('Filtra clientes por tamanho corretamente', () => {
+    const sizes = ['Small', 'Medium', 'Enterprise', 'Large Enterprise', 'Very Large Enterprise']
 
-describe('Teste de filtro de clientes por tamanho', () => {
-    it('Deve filtrar clientes por tamanho corretamente', () => {
-      cy.request('GET', 'http://localhost:3001/customers?page=1&limit=10&size=Small').then((response) => {
-
-        expect(response.status).to.equal(200);
-        expect(response.body.customers).to.have.length.gt(0);
-        response.body.customers.forEach((cliente) => {
-          expect(getSize(cliente)).to.equal('Small');
-        });
-      });
-    });
+    sizes.forEach((size) => {
+      cy.request('GET', `${CUSTOMERS_API_URL}?page=1&limit=50&size=${size}`)
+        .then(({ body }) => {
+          body.customers.forEach((customer) => {
+            expect(customer.size).to.eq(size)
+          })
+        })
+    })
   });
-
-  const getSize = ({ employees }) => {
-    if (employees >= 50000) return 'Very Large Enterprise';
-    if (employees >= 10000) return 'Large Enterprise';
-    if (employees >= 1000) return 'Enterprise';
-    if (employees >= 100) return 'Medium';
-    return 'Small';
-  };
-
- //Retorna a estrutura correta da resposta (ou seja, propriedades dos customers e pageInfo)
-
- describe('Teste de Estrutura da Resposta', () => {
-    it('Deve retornar a estrutura correta da resposta', () => {
-      const url = 'http://localhost:3001/customers?page=1&limit=50&size=All';
-
-      cy.request('GET', url).then((response) => {
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.all.keys('customers', 'pageInfo');
-        expect(response.body.customers).to.be.an('array');
-        expect(response.body.pageInfo).to.be.an('object');
-
-      });
-    });
-  });
+});
+ 
