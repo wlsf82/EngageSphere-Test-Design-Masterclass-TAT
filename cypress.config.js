777b11ecf94702d085170105f05faa6aff82e774
RalfMaitha <ralf.carneiro@maitha.com.br>
2024-05-23 21:31:55 -0300

    ajustes dos testes de API

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    "env": {
+      "API_URL": "http://localhost:3001"
+    }
   },
 });
--- cypress/e2e/API/EngageSphere.cy.js
+++ cypress/e2e/API/EngageSphere.cy.js
@@ -1,153 +1,130 @@
-const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`;
-
 /// <reference types="Cypress" />
+
+const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`;
 describe('GET /customers', () => {
     context('Solicitações inválidas são tratadas de forma elegante', () => {
       it('Retorna um erro e código de status 400 ao passar um valor inválido para o parâmetro page', () => {
         cy.request({
           method: 'GET',
-          url: 'http://localhost:3001/customers?page=invalid&limit=10&size=All',
+          url: `${CUSTOMERS_API_URL}?page=invalid&limit=10&size=All`,
           failOnStatusCode: false 
-        }).then(response => {
-          expect(response.status).to.eq(400)
-        });
-      });
+        }).then(({ status }) => {
+          expect(status).to.eq(400);
+        })
+      })
       it('Retorna um erro e código de status 400 ao passar um valor inválido para o parâmetro size', () => {
         cy.request({
           method: 'GET',
-          url: 'http://localhost:3001/customers?size=extra_large',
+          url: `${CUSTOMERS_API_URL}?size=extra_large`,
           failOnStatusCode: false
-        }).then((response) => {
-          expect(response.status).to.eq(400);
-          expect(response.body.error).to.contain('Unsupported size value');
-          expect(response.body.error).to.contain('Supported values are All, Small, Medium, Enterprise, Large Enterprise, and Very Large Enterprise.');
+        }).then(({ status,body }) => {
+          expect(status).to.eq(400);
+          expect(body.error).to.contain('Unsupported size value');
+          expect(body.error).to.contain('Supported values are All, Small, Medium, Enterprise, Large Enterprise, and Very Large Enterprise.');
         });
       });
       it('Retorna um erro e código de status 400 ao passar um valor nulo para o parâmetro page', () => {
         cy.request({
           method: 'GET',
-          url: 'http://localhost:3001/customers?page=null&limit=10&size=Small',
+          url: `${CUSTOMERS_API_URL}?page=null&limit=10&size=Small`,
           failOnStatusCode: false 
-        }).then(response => {
-          expect(response.status).to.eq(400);
+        }).then(({ status }) => {
+          expect(status).to.eq(400);
         });
       });
       it('Retorna um erro e código de status 400 ao passar um valor nulo para o parâmetro size', () => {
         cy.request({
           method: 'GET',
-          url: 'http://localhost:3001/customers?page=1&limit=10&size=null',
+          url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=null`,
           failOnStatusCode: false
-        }).then(response => {
-          expect(response.status).to.eq(400);
+        }).then(({ status }) => {
+          expect(status).to.eq(400);
         });
       })
       it('Retorna um erro e código de status 400 ao passar um boleano', () => {
         cy.request({
           method: 'GET',
-          url: 'http://localhost:3001/customers?page=1&limit=true&size=Large%20Enterprise',
+          url: `${CUSTOMERS_API_URL}?page=1&limit=true&size=Large%20Enterprise`,
           failOnStatusCode: false
-        }).then(response => {
-          expect(response.status).to.eq(400);
+        }).then(({ status }) => {
+          expect(status).to.eq(400);
           //Obs -> não retorna mensagem
         });
       });
     });
     context('Validação de paginação', () => {
+      const validatePagination = (page) => {
+        cy.request({
+          method: 'GET',
+          url: `${CUSTOMERS_API_URL}?page=${page}&limit=10&size=All`
+        }).then(({ status, body }) => {
+          expect(status).to.eq(200);
+          expect(body.pageInfo).to.be.an('object');
+          expect(body.pageInfo.currentPage).to.eq(`${page}`);
+          if (body.pageInfo.currentPage > body.pageInfo.totalPages) {
+            expect(body.customers).to.be.an('array').that.is.empty;
+          } else {
+            expect(body.customers).to.be.an('array').that.is.not.empty;
+          }
+        });
+      };
         it('Validação de paginação da primeira página deve retornar 200', () => {
-          cy.request({
-            method: 'GET',
-            url: 'http://localhost:3001/customers?page=1&limit=10&size=All'
-          }).then((response) => {
-            console.log('Clientes da primeira página:', response.body);
-            expect(response.status).to.eq(200);
-            expect(response.body.customers.length).to.eq(10);
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo.currentPage).to.eq('1');
-            expect(response.status).to.eq(200);
-            expect(response.body.customers.length).to.eq(10);
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo.currentPage).to.eq(response.body.pageInfo.currentPage);
-            expect(response.body.pageInfo.totalPages).to.eq(response.body.pageInfo.totalPages);
-            expect(response.body.pageInfo.totalCustomers).to.eq(response.body.pageInfo.totalCustomers);
-            expect(response.body.pageInfo.totalCustomers).to.be.a('number');
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo).to.have.property('currentPage');
-            expect(response.body.pageInfo.currentPage).to.be.a('string');
-            expect(response.body.pageInfo.currentPage).to.not.be.empty;
-            expect(response.body.pageInfo).to.have.property('totalPages');
-            expect(response.body.pageInfo.totalPages).to.be.a('number');
-            expect(response.body.pageInfo.totalPages).to.be.greaterThan(0);
-            expect(response.body.pageInfo).to.have.property('totalCustomers');
-            expect(response.body.pageInfo.totalCustomers).to.be.a('number');
-          });
+          validatePagination(1);
         });
       
         it('Validação de paginação da segunda página deve retornar 200', () => {
-          cy.request({
-            method: 'GET',
-            url: 'http://localhost:3001/customers?page=2&limit=10&size=All'
-          }).then((response) => {
-            console.log('Clientes da segunda página:', response.body);
-            expect(response.status).to.eq(200);
-            expect(response.body.customers.length).to.eq(10);
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo.currentPage).to.eq('2');
-            expect(response.status).to.eq(200);
-            expect(response.body.customers.length).to.eq(10);
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo.currentPage).to.eq(response.body.pageInfo.currentPage);
-            expect(response.body.pageInfo.totalPages).to.eq(response.body.pageInfo.totalPages);
-            expect(response.body.pageInfo.totalCustomers).to.eq(response.body.pageInfo.totalCustomers);
-            expect(response.body.pageInfo.totalCustomers).to.be.a('number');
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo).to.have.property('currentPage');
-            expect(response.body.pageInfo.currentPage).to.be.a('string');
-            expect(response.body.pageInfo.currentPage).to.not.be.empty;
-            expect(response.body.pageInfo).to.have.property('totalPages');
-            expect(response.body.pageInfo.totalPages).to.be.a('number');
-            expect(response.body.pageInfo.totalPages).to.be.greaterThan(0);
-            expect(response.body.pageInfo).to.have.property('totalCustomers');
-            expect(response.body.pageInfo.totalCustomers).to.be.a('number');
-          });
+          validatePagination(2);
         }); 
     
         it('Validação de paginação da última página deve retornar 200', () => {
-          cy.request({
-            method: 'GET',
-            url: 'http://localhost:3001/customers?page=5&limit=10&size=All'
-          }).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body.customers.length).to.eq(10);
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo.currentPage).to.eq('5');
-            expect(response.body.pageInfo.totalPages).to.eq(response.body.pageInfo.totalPages);
-            expect(response.body.pageInfo.totalCustomers).to.eq(response.body.pageInfo.totalCustomers);
-            expect(response.body.pageInfo.totalCustomers).to.be.a('number');
-            expect(response.body.pageInfo).to.be.an('object');
-            expect(response.body.pageInfo).to.have.property('currentPage');
-            expect(response.body.pageInfo.currentPage).to.be.a('string');
-            expect(response.body.pageInfo.currentPage).to.not.be.empty;
-            expect(response.body.pageInfo).to.have.property('totalPages');
-            expect(response.body.pageInfo.totalPages).to.be.a('number');
-            expect(response.body.pageInfo.totalPages).to.be.greaterThan(0);
-            expect(response.body.pageInfo).to.have.property('totalCustomers');
-            expect(response.body.pageInfo.totalCustomers).to.be.a('number');
-          });
-        }); 
+          validatePagination(5);
+        });
+
+        it('Validação de paginação de página vazia deve retornar 200 e nenhum cliente', () => {
+          validatePagination(6);
+        });
     });
     context('Recuperação de clientes', () => {
         it('Recupera clientes com sucesso deve retornar 200', () => {
             cy.request({
               method: 'GET',
-              url: 'http://localhost:3001/customers?page=1&limit=10&size=All',
-              headers: {
-                'If-None-Match': null,
-                'If-Modified-Since': null
-              }
-            }).then((response) => {
-              expect(response.status).to.eq(200);
+              url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=All`,
+            }).then(({ status }) => {
+              expect(status).to.eq(200);
             });
           });
+    });
+    context('Recuperação clientes filtrado por size e limit com sucesso', () => {
+      it('Retorna 200 na recuperação do cliente por Small e limite de 10', () => {
+        cy.request({
+          method: 'GET',
+          url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=Small`,
+        }).then(({ status, body }) => {
+          expect(status).to.eq(200);
+          expect(body.customers).to.be.an('array').that.is.not.empty;
+          expect(body.customers.length).to.eq(3);
+          expect(body.customers).to.have.lengthOf.at.most(10);
+          body.customers.forEach(customer => {
+            expect(customer.size).to.eq('Small');
+          });
+        });
+      });
+
+      it('Retorna 200 na recuperação do cliente por Small e limite de 10', () => {
+        cy.request({
+          method: 'GET',
+          url: `${CUSTOMERS_API_URL}?page=1&limit=5&size=Small`,
+        }).then(({ status, body }) => {
+          expect(status).to.eq(200);
+          expect(body.customers).to.be.an('array').that.is.not.empty;
+          expect(body.customers.length).to.eq(3);
+          expect(body.customers).to.have.lengthOf.at.most(10);
+          body.customers.forEach(customer => {
+            expect(customer.size).to.eq('Small');
+          });
+        });
+      });
+
     });
   });
   
