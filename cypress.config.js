271287519a5cebbd36f1c8a6e3aed25d8fa0a96b
sanychiquetti <sanymara77@hotmail.com>
2024-05-24 10:49:21 +0200

    baixando as biliotecas react na pasta raiz e correções

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
-    supportFile: false,
     env: {
       API_URL: 'http://localhost:3001/',
     },
--- frontend/src/components/Pagination.cy.js
+++ frontend/src/components/Pagination.cy.js
@@ -11,81 +11,81 @@ describe('<Pagination />', () => {
             onClickNext: cy.stub(),
             onChage: cy.stub(),
         }
+    })
 
-        it('Renderizar a página com um limite de 10 itens por página', () => {
-            const props = {
-                currentPage: 1,
-                paginationInfo: {
-                    totalPages: 5,
-                    limit: 10
-                },
-                ...defaultProps,
-            }
-            mount(<Pagination {...props} />)
+    it('Renderizar a página com um limite de 10 itens por página', () => {
+        const props = {
+            currentPage: 1,
+            paginationInfo: {
+                totalPages: 5,
+                limit: 10
+            },
+            ...defaultProps,
+        }
+        mount(<Pagination {...props} />)
 
-            cy.get('select').should('have.value', 10)
-        })
+        cy.get('select').should('have.value', 10)
+    })
 
-        it('Desativa o botão de paginação "Anterior" quando na primeira página', () => {
-            const props = {
-                currentPage: 1,
-                paginationInfo: {
-                    totalPages: 3,
-                    limit: 20
-                },
-                ...defaultProps,
-            }
+    it('Desativa o botão de paginação "Anterior" quando na primeira página', () => {
+        const props = {
+            currentPage: 1,
+            paginationInfo: {
+                totalPages: 3,
+                limit: 20
+            },
+            ...defaultProps,
+        }
 
-            mount(<Pagination {...props} />)
+        mount(<Pagination {...props} />)
 
-            cy.contains('button', 'Prev').should('be.disabled').and('be.visible')
-        })
+        cy.contains('button', 'Prev').should('be.disabled').and('be.visible')
+    })
 
-        it('Desativa o botão de paginação "Próxima" quando na última página', () => {
-            const props = {
-                currentPage: 3,
-                paginationInfo: {
-                    totalPages: 3,
-                    limit: 20
-                },
-                ...defaultProps,
-            }
+    it('Desativa o botão de paginação "Próxima" quando na última página', () => {
+        const props = {
+            currentPage: 3,
+            paginationInfo: {
+                totalPages: 3,
+                limit: 20
+            },
+            ...defaultProps,
+        }
 
-            mount(<Pagination {...props} />)
+        mount(<Pagination {...props} />)
 
-            cy.contains('button', 'Prev').should('be.disabled').and('be.visible')
-        })
+        cy.contains('button', 'Next').should('be.disabled').and('be.visible')
+    })
 
-        it('Desativa ambos os botões quando há apenas uma página', () => {
-            const props = {
-                currentPage: 1,
-                paginationInfo: {
-                    totalPages: 10,
-                    limit: 50
-                },
-                ...defaultProps,
-            }
+    it('Desativa ambos os botões quando há apenas uma página', () => {
+        const props = {
+            currentPage: 1,
+            paginationInfo: {
+                totalPages: 1,
+                limit: 50
+            },
+            ...defaultProps,
+        }
 
-            mount(<Pagination {...props} />)
+        mount(<Pagination {...props} />)
 
-            cy.contains('button', 'Prev').should('be.disabled').and('be.visible')
-            cy.contains('button', 'Next').should('be.disabled').and('be.visible')
-            cy.contains('span', 'Page 1 of 1').should('be.visible')
-        })
+        cy.contains('button', 'Prev').should('be.disabled').and('be.visible')
+        cy.contains('button', 'Next').should('be.disabled').and('be.visible')
+        cy.contains('span', 'Page 1 of 1').should('be.visible')
+    })
 
-        it('Mostra "Page 1 of 3"', () => {
-            const props = {
-                currentPage: 1,
-                paginationInfo: {
-                    totalPages: 3,
-                    limit: 20
-                },
-                ...defaultProps,
-            }
+    it('Mostra "Page 1 of 3"', () => {
+        const props = {
+            currentPage: 1,
+            paginationInfo: {
+                totalPages: 3,
+                limit: 20
+            },
+            ...defaultProps,
+        }
 
-            mount(<Pagination {...props} />)
+        mount(<Pagination {...props} />)
 
-            cy.contains('span', 'Page 1 of 3').should('be.visible')
-        })
+        cy.contains('span', 'Page 1 of 3').should('be.visible')
     })
 })
