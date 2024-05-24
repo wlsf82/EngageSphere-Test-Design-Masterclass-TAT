358968534dc85047674f63dab302c61c141ff6fb
RalfMaitha <ralf.carneiro@maitha.com.br>
2024-05-23 22:32:13 -0300

    ajuste sobre validacao elemento DOM

--- cypress/e2e/GUI/render_h1_and_theme.cy.js
+++ cypress/e2e/GUI/render_h1_and_theme.cy.js
@@ -3,31 +3,19 @@ describe('Elementos de tela', () => {
     beforeEach(() => {
         cy.visit('/')
     });
-    context('Quando valido o cabeçalho como um h1', () => {
-        it('Então deve o cabeçalho deve conter com um h1', () => {
+    context('Quando valido o título', () => {
+        it('Deve conter EngageSphere como título', () => {
             cy.wait('@pag1limit10all')
-            cy.get('h1').then(($confirmElement) => {
-                if($confirmElement.length > 0) {
-                    cy.get('h1').should('exist');
-                }
-            })
+            cy.contains('h1', 'EngageSphere').should('be.visible');
         });
     });
     context('Quando valido o cabeçalho como um h1 com alternância de tema', () => {
         beforeEach(() => {
-            cy.get('#theme-toggle-button').then(($confirmElement) => {
-                if($confirmElement.length > 0) {
-                    cy.get('#theme-toggle-button').click()
-                }
-            })
+            cy.get('#theme-toggle-button').click()
         });
         it('Então deve o cabeçalho com alternância de tema deve conter com um h1', () => {
             cy.wait('@pag1limit10all')
-            cy.get('h1').then(($confirmElement) => {
-                if($confirmElement.length > 0) {
-                    cy.get('h1').should('exist');
-                }
-            });
+            cy.contains('h1', 'EngageSphere').should('be.visible');
         });
     }); 
 });
